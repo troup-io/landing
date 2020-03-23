931b2cd96397512fce1127f117f1e8b4874b8dda
Samrith Shankar <samrith@thetotem.co>
2020-03-23 01:56:08 +0100

    Added first iteration of content

---
+++ components/Content.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import styles from '../styles/main.scss';
+
+export default function Content() {
+    return (
+        <main>
+            <h1>Simplifying everything about teamwork.</h1>
+            <h2>
+                Troup is an open source collaboration platform that helps teams
+                <br />
+                complete projects seamlessly.
+            </h2>
+            <h3>Coming soon.</h3>
+            <div className={styles.button_group}>
+                <button className={styles.button_group__button}>Join Chat</button>
+                <button
+                    className={`${styles.button_group__button} ${styles.button_group__button__primary}`}
+                >
+                    Contribute
+                </button>
+                <button className={styles.button_group__button}>Features</button>
+            </div>
+            <img
+                src="images/people.svg"
+                className={styles.people}
+                alt="Troup inculcating a mindset in people."
+                width="50%"
+            />
+        </main>
+    );
+}
