44e725562433f62484403e8b5d6cd4643eef69a7
Samrith Shankar <samrith@thetotem.co>
2020-03-23 01:55:57 +0100

    Added first iteration of header

---
+++ components/Header.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import styles from '../styles/main.scss';
+
+export default function Header() {
+    return (
+        <header>
+            <a href="/">
+                <img src="images/header-logo.png" width="120" alt="Troup Logo" />
+            </a>
+
+            <ul className={styles.nav}>
+                <a className={styles.nav__item} href="https://github.com/troup-io">
+                    <li>Docs</li>
+                </a>
+                <a className={styles.nav__item} href="https://github.com/troup-io">
+                    <li>Contributing</li>
+                </a>
+                <a className={styles.nav__item} href="https://spectrum.chat/troup">
+                    <li>Join Chat</li>
+                </a>
+            </ul>
+        </header>
+    );
+}
