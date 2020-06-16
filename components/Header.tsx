ff3b5a3c6bd4765f3e39b42e70172cd8f605d401
Samrith Shankar <samrith.298@gmail.com>
2020-06-17 00:12:36 +0200

    Simplify Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,36 +7,8 @@ export default function Header() {
     return (
         <header>
             <a href="/">
-                <img src="images/header-logo.png" alt="Troup Logo" className={styles.logo} />
+                <img src="images/header-logo.svg" alt="Troup Logo" className={styles.logo} />
             </a>
-
-            <ul className={styles.nav}>
-                <a className={styles.nav__item} href="https://github.com/troup-io">
-                    <li>Features</li>
-                </a>
-                <a
-                    className={classes(
-                        styles.nav__item,
-                        styles.nav__item__primary,
-                        styles.nav__item__mobile
-                    )}
-                    href="https://github.com/troup-io"
-                >
-                    <li>Contribute</li>
-                </a>
-                <a
-                    className={styles.nav__item}
-                    href="https://github.com/orgs/troup-io/teams/discussions"
-                >
-                    <li>Join Chat</li>
-                </a>
-                <a
-                    className={classes(styles.nav__item, styles.nav__item__primary)}
-                    href="https://github.com/troup-io"
-                >
-                    <li>Contribute</li>
-                </a>
-            </ul>
         </header>
     );
 }
