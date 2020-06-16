c27412dc00d4ec05494eb7c1992ff28e03a900ca
Samrith Shankar <samrith.298@gmail.com>
2020-06-17 00:12:30 +0200

    Update content

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,51 +1,48 @@
 import React from 'react';
 
 import styles from '../styles/main.scss';
+
 import { classes } from '../utils';
 
 export default function Content() {
     return (
         <main>
-            <h1>Simplifying everything about teamwork.</h1>
-            <h2>
-                Troup is an open source collaboration platform that helps teams
-                <br />
-                complete projects seamlessly.
-            </h2>
-            <h3>Coming soon.</h3>
-            <div className={styles.button_group}>
-                <a
-                    href="https://github.com/troup-io"
-                    className={classes(
-                        styles.button_group__button,
-                        styles.button_group__button__primary,
-                        styles.button_group__button__primary__mobile
-                    )}
-                >
-                    Contribute
-                </a>
-                <a
-                    href="https://github.com/orgs/troup-io/teams/discussions"
-                    className={styles.button_group__button}
-                >
-                    Join Chat
-                </a>
-                <a
-                    href="https://github.com/troup-io/troup-server"
-                    className={classes(
-                        styles.button_group__button,
-                        styles.button_group__button__primary
-                    )}
-                >
-                    Contribute
-                </a>
-                <a className={styles.button_group__button}>Features</a>
+            <div className={styles.half}>
+                <h1>Simplifying everything about teamwork.</h1>
+                <h2>
+                    Troup is an <strong>upcoming</strong> open source collaboration platform that
+                    helps teams complete projects seamlessly.
+                </h2>
+                <ul data-hide="tablet-lower">
+                    <li>
+                        Spend <strong>no time</strong> setting up a process for your organization
+                    </li>
+                    <li>
+                        A <strong>clear outline</strong> of your deliverables with transparency
+                    </li>
+                    <li>
+                        <strong>Excellent collaboration</strong> between various teams through your
+                        content
+                    </li>
+                </ul>
             </div>
-            <img
-                src="images/people.svg"
-                className={styles.people}
-                alt="Troup inculcating a mindset in people."
-            />
+            <div className={styles.half}>
+                <img src="/images/people.svg" alt="People" />
+            </div>
+            <div className={classes(styles.half, styles.halfMobile)}>
+                <ul>
+                    <li>
+                        Spend <strong>no time</strong> setting up a process for your organization
+                    </li>
+                    <li>
+                        A <strong>clear outline</strong> of your deliverables with transparency
+                    </li>
+                    <li>
+                        <strong>Excellent collaboration</strong> between various teams through your
+                        content
+                    </li>
+                </ul>
+            </div>
         </main>
     );
 }
