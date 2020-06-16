4f598e666d17bd47d331061b308e3f44ff7231b0
Samrith Shankar <samrith.298@gmail.com>
2020-06-17 00:12:48 +0200

    Add richness to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,33 @@
 import Head from 'next/head';
 
+import Background from '../components/Background';
 import Header from '../components/Header';
 import Content from '../components/Content';
 
-import '../styles/main.scss';
+import styles from '../styles/main.scss';
 
 export default function Home() {
     return (
-        <section>
-            <Head>
-                <title>Troup - Simplifying everything about teamwork.</title>
-                <link rel="icon" href="/favicon.ico" />
-                <meta
-                    name="viewport"
-                    content="initial-scale=1.0, width=device-width"
-                    key="viewport"
-                />
-            </Head>
-            <Header />
-            <Content />
-        </section>
+        <div id={styles.wrapper}>
+            <Background />
+            <div id={styles.root}>
+                <Head>
+                    <title>Troup - Simplifying everything about teamwork.</title>
+                    <link rel="icon" href="/favicon.ico" />
+                    <meta
+                        name="viewport"
+                        content="initial-scale=1.0, width=device-width"
+                        key="viewport"
+                    />
+                </Head>
+                <Header />
+                <Content />
+                <footer>
+                    <a href="http://hub.troup.io">GitHub</a>
+                    <a href="http://chat.troup.io">Chat</a>
+                    <a href="http://twitter.com/troup_app">Twitter</a>
+                </footer>
+            </div>
+        </div>
     );
 }
